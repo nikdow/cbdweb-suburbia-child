9496a91a309c24d8aa0d49fbf3110b2ac6b00cea
cbdweb <nik@cbdweb.net>
2019-01-08 00:20:51 +1100

    Remove manifest

--- header.php
+++ header.php
@@ -20,7 +20,6 @@
     <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/manifest.json">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f2edde">
     <meta name="theme-color" content="#f2edde">
     
