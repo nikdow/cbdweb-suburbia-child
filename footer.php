00814c9398e449301252e6526797e095f52f8d7e
nikdow <nik@cbdweb.net>
2022-06-29 11:43:32 +1000

    modify footer credit links;

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</div>
 		<div class="footer clear">
         	<p>&copy; <?php the_time(__('Y')) ?> <?php bloginfo('name'); ?>. All Rights Reserved.</p>
-            <p class="about">Theme modified by CBDWeb from Suburbia, thanks to <a href="http://www.wpshower.com" title="WPSHOWER">WPSHOWER</a> & <a href="http://www.moodyguy.net/">MOODYGUY</a></p>
+            <p class="about">Theme modified by CBDWeb from Suburbia, thanks to <a href="http://www.wpshower.com" title="WPSHOWER">WPSHOWER</a></p>
         </div>
 	</div><!-- #wrapper -->
 <?php wp_footer(); ?>
